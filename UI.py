688a226d73fed21f9f2b4e55e060ea4d4ec21d9d
KranosRG <kranos401@gmail.com>
2025-09-27 15:46:52 -0400

    Will take user's ingredients input and display them in a list

---
+++ UI.py
@@ -0,0 +1,19 @@
+def IngredientsInput():
+        ingredients = []
+        print("Enter your ingredients one at a time (type 'done!' to finish):")
+
+        while True:
+                item = input("Ingredient: ").strip().lower()
+                if item == 'done':
+                    break
+        
+                if item.isnumeric():   
+                    print("Error: ingredient names cannot be numbers. Please enter text.")
+                    continue  
+                
+                if item:
+                    ingredients.append(item)
+
+        return ingredients
+ingredients = IngredientsInput()
+print(ingredients)
